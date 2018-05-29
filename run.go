5e9b376fa7da80292932c27909ab1322b1eaa1a2
Waldz <valdas@mysterium.network>
2018-05-29 17:28:06 +0300

    Build Docker image

--- run.go
+++ run.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 	"runtime"
+	"log"
 )
 
 const IS_WINDOWS = runtime.GOOS == "windows"
@@ -17,70 +18,89 @@ Usage:
 
 The commands are:
 	test      runs all tests in project
-	build     builds server and client applications to 'out' directory
+	build     builds applications to 'out' directory
 	server    builds and runs server app
 	client    builds and runs client app
 	web       builds and runs client app with integrated website
 `)
 }
 
-func build(name, folder string) string {
+func build(binaryName, packageName string) {
 	if IS_WINDOWS {
-		name += ".exe"
+		binaryName += ".exe"
 	}
-	core.Execute("go build -i -o ./out/" + name + " " + folder)
-	fmt.Println("Build successful: out/" + name)
-	return name
+	core.MustRunCommand("go", "build", "-i", "-o", "./out/" +binaryName, packageName)
+	fmt.Printf("Build successful: out/%s\n", binaryName)
 }
 
-func buildServer() string {
-	return build("server", "./server")
+func buildServer() {
+	build("server", "./server")
 }
 
-func buildClient() string {
-	return build("client", "./client")
+func buildServerImage() {
+	buildDocker("docker/server/Dockerfile", "mysteriumnetwork/winter-server")
 }
 
-func buildWeb() string {
-	name := build("web", "./client_web")
+func buildClient() {
+	build("client", "./client")
+}
+
+func buildWeb() {
+	build("web", "./client_web")
 	if IS_WINDOWS {
 		core.Execute("xcopy /f /y client_web\\html out\\html")
 	} else {
 		core.Execute("cp -a ./client_web/html ./out")
 	}
-
-	return name
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		showHelp()
-		return
-	}
+func buildDocker(dockerfile, image string) {
+	fmt.Printf("Building image '%s'..\n", image)
 
-	var run string
-	switch strings.ToLower(os.Args[1]) {
-	case "test":
-		core.Execute("go test ./...")
+	core.MustRunCommand("docker", "build", "--file", dockerfile, "--tag", image, ".")
+	fmt.Print("Docker image building process complete!")
+}
 
-	case "build":
+func doBuild(args []string) {
+	switch strings.ToLower(args[0]) {
+	case "server":
+		buildServer()
+	case "client":
+		buildClient()
+	case "web":
+		buildWeb()
+	case "server-image":
+		buildServerImage()
+	default:
 		buildServer()
 		buildClient()
 		buildWeb()
+		buildServerImage()
+	}
+}
 
-	case "server":
-		run = buildServer()
-	case "client":
-		run = buildClient()
-	case "web":
-		run = buildWeb()
-
-	case "help":
-		fallthrough
-	default: showHelp()
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("need subcommand as first argument")
 	}
 
-	if run != "" {
-		core.ExecuteIn("./" + run, "./out")
+	switch strings.ToLower(os.Args[1]) {
+	case "test":
+		core.Execute("go test ./...")
+	case "build":
+		doBuild(os.Args[2:])
+	case "server":
+		buildServer()
+		core.ExecuteIn("./server", "./out")
+	case "client":
+		buildClient()
+		core.ExecuteIn("./client", "./out")
+	case "web":
+		buildWeb()
+		core.ExecuteIn("./web", "./out")
+	case "help":
+		fallthrough
+	default:
+		showHelp()
 	}
 }
